3a3d0f1d2f1007318aff87265dac6dcc38841711
alejandraroobles3 <64538989+alejandraroobles3@users.noreply.github.com>
2023-11-07 12:48:54 -0600

    extract facebook comments in just one post
    fb comments

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import facebook_scraper as fs
+import csv
+from time import sleep
 
 # get POST_ID from the URL of the post which can have the following structure:
 # https://www.facebook.com/USER/posts/POST_ID
 # https://www.facebook.com/groups/GROUP_ID/posts/POST_ID
-POST_ID = "pfbid02NsuAiBU9o1ouwBrw1vYAQ7khcVXvz8F8zMvkVat9UJ6uiwdgojgddQRLpXcVBqYbl"
+POST_ID = "pfbid02X9A5QtZugbe5Sn5gUhoq17EMH4TC5vCZ7spo3kLifRg4EA1LKKF9xhELgsP5V1bKl"
 
 # number of comments to download -- set this to True to download all comments
-MAX_COMMENTS = 100
+MAX_COMMENTS = 50
 
 # get the post (this gives a generator)
 gen = fs.get_posts(
@@ -20,12 +22,17 @@ post = next(gen)
 # extract the comments part
 comments = post['comments_full']
 
+file=open('claudia.csv',mode='a')
+
 # process comments as you want...
+coment_txt=[]
 for comment in comments:
+    
+    print(comment['comment_text'])
+    coment_txt.append(comment['comment_text'])
 
-    # e.g. ...print them
-    print(comment)
+print("este es el segundo comentario : \n",len(coment_txt))
 
-    # e.g. ...get the replies for them
-    for reply in comment['replies']:
-        print(' ', reply)
+with open('claudia.txt', 'a', encoding='utf-8') as file:
+    for item in coment_txt:
+        file.write(item+ '\n')
