71098d976cc42fdfd6cc5cd061133aae20766afb
alejandraroobles3 <64538989+alejandraroobles3@users.noreply.github.com>
2023-11-22 13:43:11 -0600

    comments and twitter script

--- inprovisado.py
+++ inprovisado.py
@@ -1,1 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
+# Replace with the URL of the tweet you want to scrape
+tweet_url = 'https://twitter.com/user/status/1234567890123456789'
+
+# Set up Selenium WebDriver
+driver = webdriver.Chrome()  # You'll need to have the ChromeDriver executable in your PATH
+driver.get(tweet_url)
+time.sleep(5)  # Wait for the page to load
+
+# Scroll down to load more replies
+body = driver.find_element_by_tag_name('body')
+for _ in range(3):  # Adjust the number of scrolls based on the number of replies you want to load
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(2)
+
+# Get comments (replies)
+comments = driver.find_elements_by_css_selector('.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-5f2r5o.r-1mi0q7o')
+comment_texts = [comment.text for comment in comments]
+
+# Print and write comments to a file
+with open('txt/tweets_selenium.txt', 'a', encoding='utf-8') as file:
+    for item in comment_texts:
+        print(item)
+        file.write(item + '\n')
+
+# Close the browser window
+driver.quit()
--- main.py
+++ main.py
@@ -7,26 +7,15 @@ from time import sleep
 # https://www.facebook.com/groups/GROUP_ID/posts/POST_ID"
 
 """ pos id ya usados
-pfbid02ZBYoe3rWzDrTeaM2GshtBCZZDcG8Vzv6TFyEGZdzXxBSaUjuSBGVCgnmnqeuauwzl
-pfbid028NVERViFrtRXduuN4QGk4WrdD7MffrSaYH8sWwrBrALxoFDSoSNjKaeSFS3gGB8Cl
-pfbid02Po8tMkW14hKEoq1Yr4ze6BmQfHvcuFK1drrgbpF6P45gEPaXXWunpKt6gKzCGeBxl
-pfbid0cSp1WVi7HPRFWGV2u5Jtqswku2rpFX5Bo5wjzmcKDReAaL1DdctYHTxuN5RwfM25l
-pfbid02ATAUzD47sRNe7UYBSWj8nDsN29vAPwP5xmGTWawR7zyqhkdpnRBASh2M3TBMv7fJl
-pfbid02K1XMqLKoJv9HuFaNWQdFENvsH3tYab52LxA5X6o5oHHXGKdbayUjqFgtURe6Xj1Hl
-pfbid0KbVMMawsTnjruvbnJqAnwmxKgZf9eDwM4tM8RUmysoHyArrHoQUboXVxB6p5UHhAl
-pfbid0BUFMzJuTPR63EK4vAGEPJNPogpJTgCzjQEo4V5s3RnSSsj13nZ1qTfpV7qrmQE34l
-pfbid02vfdsSVP1gYmdhf4nVR8Vd8jRH7MTUvyimvyDUXRKKjxszwtqrvGtqNbFQPbwfZAWl
-pfbid02QikyKG5LtjZBYaz1PK3EYLrEE65AbkmSCCNiZDPisWv6oWbZFe24ZY7Aips7ymdGl
-pfbid0DRwFVHaqpdx1pEFwsevPx9cB2jEGPNjVbVG9JaQb2RpEHay7Fq3F3cVpu8FirgxMl
-pfbid02Jm7GjpLec1JPto6Vr2ankZNRQSMDz6A4AEiNrwavaHmAHSJZ3zPxMpaNMYCwU9Yvl
-pfbid0B8NYKRYURXnVQL5zVzCQRn7jSrEiEP5Wvj2At5hQmXJ5gTMZaSkTZBjEdXrcYZYGl
-pfbid0FZztMWxpT4Y1Jz7eQnxEoaZ2GnEVvkk65KL3KKHLApVgQfLFUy42Fg8QcCPTz5MPl
+
 """
 
-POST_ID = "pfbid0FZztMWxpT4Y1Jz7eQnxEoaZ2GnEVvkk65KL3KKHLApVgQfLFUy42Fg8QcCPTz5MPl"
+POST_ID = "pfbid02Gqc6L8PraN5F6MTgZ8gJUpG9iTSipbfYCgdksd6DCrRUP6Hoj4jH1cZeqxj7HYcJl"
 
 # number of comments to download -- set this to True to download all comments
-MAX_COMMENTS = 100
+MAX_COMMENTS = 500
+
+print(POST_ID)
 
 # get the post (this gives a generator)
 gen = fs.get_posts(
@@ -51,6 +40,6 @@ for comment in comments:
 
 #print("este es el segundo comentario : \n",len(coment_txt))
 
-with open('cucei.txt', 'a', encoding='utf-8') as file:
+with open('txt/udg.txt', 'a', encoding='utf-8') as file:
     for item in coment_txt:
         file.write(item+ '\n')
